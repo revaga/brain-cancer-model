45218371e5fe6dee8c764c86c7610ade24c8be7e
antsfly626 <nehahingorani06@gmail.com>
2025-02-17 19:49:25 -0800

    uploaded dataset and created main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# creating CNN to predict brain cancer
+
+# importing libraries
+import numpy as np
+import pandas as pd
+import cv2
+import matplotlib.pyplot as plt
+import torch
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import Dataset, DataLoader
+import os
+
+# create dataset class
+class BrainTumorDataset(Dataset):
+
+
