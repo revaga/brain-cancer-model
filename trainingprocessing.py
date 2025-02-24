996b455c157dff9f1022c27feb50a1fdd8a9af97
antsfly626 <nehahingorani06@gmail.com>
2025-02-23 17:14:29 -0800

    converted testing images to tensors

--- trainingprocessing.py
+++ trainingprocessing.py
@@ -53,6 +53,40 @@ for i in range(10,1457): #1456 images, first 10 ommitted
     #print(training_pituitary[i])
 
 
+testing_glioma = []
+for i in range(10, 300): #300 images, skipped first 10
+    val = str(i)
+    while(len(val) !=4):
+        val = "0"+val
+    testing_glioma.append("Training/glioma/Tr-gl_" + val+ ".jpg")
+
+    
+testing_meningioma = []
+for i in range(10, 306): #306 images, skipped first 10
+    val = str(i)
+    while(len(val) !=4):
+        val = "0"+val
+    testing_meningioma.append("Training/meningioma/Tr-me_" + val+ ".jpg")
+
+    
+testing_notumor = []
+for i in range(10,405): #405 images, skipped first 10
+    val = str(i)
+    while(len(val) != 4):
+        val = "0" + val
+    testing_notumor.append("Training/notumor/Tr-no_" + val+ ".jpg")
+    #print("Training/notumor/Tr-no_" + val+ ".jpg")
+
+
+
+testing_pituitary = []
+for i in range(10,300): #300 images, skipped first 10
+    val = str(i)
+    while(len(val) != 4):
+        val = "0" + val
+    testing_pituitary.append("Training/pituitary/Tr-pi_" + val+ ".jpg")
+    #print(testing_pituitary[i])
+
 """
 #showing images
 
@@ -109,4 +143,29 @@ for i in range(0, len(training_pituitary)):
     traintensor_pituitary.append(img_tensor)
 
 
+testtensor_glioma = []
+for i in range(0, len(testing_glioma)):
+    img = Image.open(testing_glioma[i])
+    img_tensor = transform(img)
+    #print(img_tensor)
+    testtensor_glioma.append(img_tensor)
 
+testtensor_meningioma = []
+for i in range(0, len(testing_meningioma)):
+    img = Image.open(testing_meningioma[i])
+    img_tensor = transform(img)
+    #print(img_tensor)
+    testtensor_meningioma.append(img_tensor)
+
+testtensor_notumor = []
+for i in range(0, len(testing_notumor)):
+    img = Image.open(testing_notumor[i])
+    img_tensor = transform(img)
+    testtensor_notumor.append(img_tensor)
+
+testtensor_pituitary = []
+for i in range(0, len(testing_pituitary)):
+    img = Image.open(testing_pituitary[i])
+    img_tensor = transform(img)
+    #print(img_tensor)
+    testtensor_pituitary.append(img_tensor)
